f730408f860c9716897b1f865eba6bfe83df7f5e
Shelgachev Vadim <Shelgachev@yahoo.com>
2021-04-19 10:52:26 -0700

    before refactoring

--- build/routes/loginRoute.js
+++ build/routes/loginRoute.js
@@ -2,12 +2,41 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 var express_1 = require("express");
+function requireAuth(req, res, next) {
+    if (req.session && req.session.loggedIn) {
+        next();
+        return;
+    }
+    res.status(403);
+    res.send('Not permited');
+}
 var router = express_1.Router();
 exports.router = router;
-router.get('/login', function (rec, res) {
+router.get('/login', function (req, res) {
     res.send("\n      <form method=\"POST\">\n        <div>\n          <lable>Email</lable>\n          <input name=\"email\"/>\n        </div>\n        <div>\n          <lable>Password</lable>\n          <input name=\"password\" type=\"password\"/>\n        </div>\n        <button>Submit</button>\n      </form>\n      <h1>Hi there from router</h1>\n    ");
 });
-router.post('/login', function (rec, res) {
-    var _a = rec.body, email = _a.email, password = _a.password;
-    res.send(email.toUpperCase());
+router.post('/login', function (req, res) {
+    var _a = req.body, email = _a.email, password = _a.password;
+    if (email && password && email === 'hi@hi.com' && password === 'pa') {
+        req.session = { loggedIn: true };
+        res.redirect('/');
+    }
+    else {
+        res.send('Invalid email or password');
+    }
+});
+router.get('/', function (req, res) {
+    if (req.session && req.session.loggedIn) {
+        res.send("\n        <div>            \n           <div>You are logged in</div>\n           <a href=\"/logout\">Logout</a> \n        </div>        \n      ");
+    }
+    else {
+        res.send("\n        <div>            \n           <div>You are not logged in</div>\n           <a href=\"/login\">Login</a> \n        </div>        \n      ");
+    }
+});
+router.get('/logout', function (req, res) {
+    req.session = undefined;
+    res.redirect('/');
+});
+router.get('/protected', requireAuth, function (req, res) {
+    res.send('Welcome to protected rout, legged in uder');
 });
--- src/routes/loginRoute.ts
+++ src/routes/loginRoute.ts
@@ -1,8 +1,21 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, NextFunction } from 'express';
+
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined };
+}
+
+function requireAuth(req: Request, res: Response, next: NextFunction): void {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+  res.status(403);
+  res.send('Not permited');
+}
 
 const router = Router();
 
-router.get('/login', (rec: Request, res: Response) => {
+router.get('/login', (req: Request, res: Response) => {
   res.send(
     `
       <form method="POST">
@@ -21,9 +34,45 @@ router.get('/login', (rec: Request, res: Response) => {
   );
 });
 
-router.post('/login', (rec: Request, res: Response) => {
-  const { email, password } = rec.body;
-  res.send(email.toUpperCase());
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const { email, password } = req.body;
+  if (email && password && email === 'hi@hi.com' && password === 'pa') {
+    req.session = { loggedIn: true };
+    res.redirect('/');
+  } else {
+    res.send('Invalid email or password');
+  }
+});
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(
+      `
+        <div>            
+           <div>You are logged in</div>
+           <a href="/logout">Logout</a> 
+        </div>        
+      `
+    );
+  } else {
+    res.send(
+      `
+        <div>            
+           <div>You are not logged in</div>
+           <a href="/login">Login</a> 
+        </div>        
+      `
+    );
+  }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+  req.session = undefined;
+  res.redirect('/');
+});
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send('Welcome to protected rout, legged in uder');
 });
 
 export { router };
